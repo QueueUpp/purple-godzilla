00788739158f86dce824f850b156243f6b13094f
QueueUpp <paulkvincent99@gmail.com>
2025-08-16 15:25:20 -0500

    Add initial bot main.py

---
+++ Cooking Bot/main.py
@@ -0,0 +1,11 @@
+import time
+
+def main():
+    print("Purple Godzilla RuneScape 3 Bot Starting...")
+    # TODO: Add your bot logic here
+    while True:
+        print("Bot running...")
+        time.sleep(5)
+
+if __name__ == "__main__":
+    main()
